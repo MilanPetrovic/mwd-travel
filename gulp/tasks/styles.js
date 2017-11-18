767ee025353c29833957a203a2ee2841a2a97101
MilanPetrovic <micko.kv@gmail.com>
2017-11-19 00:47:22 +0100

    Responsive images, mixins & first page section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,14 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     postcssvars = require('postcss-simple-vars'),
     nestedcss = require('postcss-nested'),
-    postcssImport = require('postcss-import');
+    postcssImport = require('postcss-import'),
+    mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([postcssImport, postcssvars, nestedcss, autoprefixer]))
-        .on('error', function() {
+        .pipe(postcss([postcssImport, mixins, postcssvars, nestedcss, autoprefixer]))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
             this.emit('end');
         })
         .pipe(gulp.dest('./app/temp/styles'));
